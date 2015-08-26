f14e68a5350711bd784706af38e17cba2712b176
Nan Li <linan7788626@gmail.com>
2015-08-26 12:53:37 -0500

    useable icic function in CL

--- wcic_cl/test_opencl_lensing.c
+++ wcic_cl/test_opencl_lensing.c
@@ -10,8 +10,12 @@
 
 #include "all_cv_test.h"
 
-void call_kernel(float *xi1,float *xi2,int count,float *lpar,float *alpha1,float *alpha2,char * cl_name) {
-
+void call_kernel_icic(float *source_map,float *posy1,float *posy2, float ysc1, float ysc2, float dsi,
+		int nsx,int nsy,int nlx,int nly,float *lensed_map,char * cl_name) {
+//----------------------------------------------------------------------------
+// Initialization
+	int counts = nsx*nsy;
+	int countl = nlx*nly;
     FILE* programHandle;
     size_t programSize, KernelSourceSize;
     char *programBuffer, *KernelSource;
@@ -24,21 +28,21 @@ void call_kernel(float *xi1,float *xi2,int count,float *lpar,float *alpha1,float
     cl_command_queue commands;          // compute command queue
     cl_program program;                 // compute program
     cl_kernel kernel;                   // compute kernel
-
+//----------------------------------------------------------------------------
+// Claim Variables for Device
     cl_mem input1;                       // device memory used for the input array
     cl_mem input2;                       // device memory used for the input array
-    cl_mem lpar_d;                       // device memory used for the input array
-    cl_mem output1;                      // device memory used for the output array
-    cl_mem output2;                      // device memory used for the output array
-
+    cl_mem input3;                       // device memory used for the input array
+    cl_mem output;                      // device memory used for the output array
+//----------------------------------------------------------------------------
+// Setup Context of OpenCL
 	int err;
     int gpu = 1;
     err = clGetDeviceIDs(NULL, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
     context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
     commands = clCreateCommandQueue(context, device_id, 0, &err);
-
-	//----------------------------------------------------------------------------
-    // get size of kernel source
+//----------------------------------------------------------------------------
+// Read Kernel Functions from Source Files
     programHandle = fopen(cl_name, "r");
     fseek(programHandle, 0, SEEK_END);
     programSize = ftell(programHandle);
@@ -49,49 +53,57 @@ void call_kernel(float *xi1,float *xi2,int count,float *lpar,float *alpha1,float
     fread(programBuffer, sizeof(char), programSize, programHandle);
     fclose(programHandle);
 
-    // create program from buffer
     program = clCreateProgramWithSource(context,1,(const char**) &programBuffer,&programSize, NULL);
     free(programBuffer);
 
-    // read kernel source back in from program to check
     clGetProgramInfo(program, CL_PROGRAM_SOURCE, 0, NULL, &KernelSourceSize);
     KernelSource = (char*) malloc(KernelSourceSize);
     clGetProgramInfo(program, CL_PROGRAM_SOURCE, KernelSourceSize, KernelSource, NULL);
 
     program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource, NULL, &err);
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-    kernel = clCreateKernel(program, "square", &err);
-	//----------------------------------------------------------------------------
-
-    input1 = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * count, NULL, NULL);
-    input2 = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * count, NULL, NULL);
-    lpar_d = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * 6, NULL, NULL);
-    output1 = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * count, NULL, NULL);
-    output2 = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * count, NULL, NULL);
-
-    err = clEnqueueWriteBuffer(commands, input1, CL_TRUE, 0, sizeof(float) * count, xi1, 0, NULL, NULL);
-    err = clEnqueueWriteBuffer(commands, input2, CL_TRUE, 0, sizeof(float) * count, xi2, 0, NULL, NULL);
-    err = clEnqueueWriteBuffer(commands, lpar_d, CL_TRUE, 0, sizeof(float) * 6, lpar, 0, NULL, NULL);
-
+    kernel = clCreateKernel(program, "icic_cl", &err);
+//----------------------------------------------------------------------------
+// Allocate Memory for Device
+    input1 = clCreateBuffer(context, CL_MEM_READ_ONLY,  sizeof(float) * counts, NULL, NULL);
+    input2 = clCreateBuffer(context, CL_MEM_READ_ONLY,  sizeof(float) * countl, NULL, NULL);
+    input3 = clCreateBuffer(context, CL_MEM_READ_ONLY,  sizeof(float) * countl, NULL, NULL);
+    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * countl, NULL, NULL);
+//----------------------------------------------------------------------------
+// Copy Data to Device
+    err = clEnqueueWriteBuffer(commands, input1, CL_TRUE, 0, sizeof(float) * counts, source_map, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(commands, input2, CL_TRUE, 0, sizeof(float) * countl, posy1, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(commands, input3, CL_TRUE, 0, sizeof(float) * countl, posy2, 0, NULL, NULL);
+//----------------------------------------------------------------------------
+// Passing Parameters into Kernel Functions
     clSetKernelArg(kernel, 0, sizeof(cl_mem), &input1);
     clSetKernelArg(kernel, 1, sizeof(cl_mem), &input2);
-    clSetKernelArg(kernel, 2, sizeof(cl_mem), &lpar_d);
-    clSetKernelArg(kernel, 3, sizeof(cl_mem), &output1);
-    clSetKernelArg(kernel, 4, sizeof(cl_mem), &output2);
-    clSetKernelArg(kernel, 5, sizeof(int), &count);
-
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), &input3);
+    clSetKernelArg(kernel, 3, sizeof(float), &ysc1);
+    clSetKernelArg(kernel, 4, sizeof(float), &ysc1);
+    clSetKernelArg(kernel, 5, sizeof(float), &dsi);
+    clSetKernelArg(kernel, 6, sizeof(int), &nsx);
+    clSetKernelArg(kernel, 7, sizeof(int), &nsy);
+    clSetKernelArg(kernel, 8, sizeof(int), &nlx);
+    clSetKernelArg(kernel, 9, sizeof(int), &nly);
+    clSetKernelArg(kernel,10, sizeof(cl_mem), &output);
+//----------------------------------------------------------------------------
+// Runing Kernel Functions
     err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
-    global = count;
+    global = countl;
     err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
     clFinish(commands);
-    err = clEnqueueReadBuffer( commands, output1, CL_TRUE, 0, sizeof(float) * count, alpha1, 0, NULL, NULL );
-    err = clEnqueueReadBuffer( commands, output2, CL_TRUE, 0, sizeof(float) * count, alpha2, 0, NULL, NULL );
-
+//----------------------------------------------------------------------------
+// Output Array
+    err = clEnqueueReadBuffer( commands, output, CL_TRUE, 0, sizeof(float) * countl, lensed_map, 0, NULL, NULL );
+//----------------------------------------------------------------------------
+// Free the Memory in Device
     clReleaseMemObject(input1);
     clReleaseMemObject(input2);
-    clReleaseMemObject(lpar_d);
-    clReleaseMemObject(output1);
-    clReleaseMemObject(output2);
+    clReleaseMemObject(input3);
+    clReleaseMemObject(output);
+//----------------------------------------------------------------------------
+
     clReleaseProgram(program);
     clReleaseKernel(kernel);
     clReleaseCommandQueue(commands);
